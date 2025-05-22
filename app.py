d8b079b36a8cecd626997889ba0b89a50c9330d8
Sami Sheikh <samisheikh@Mac.attlocal.net>
2025-05-22 16:39:35 -0400

    Final fix: use host 0.0.0.0 and Render-assigned port

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def recommend():
     return jsonify(data[best_match_index])
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
